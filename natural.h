9f2d93fa54dbeda69c247d7081ec1c062d4c0189
maciejlup <27m.lupina@gmail.com>
2026-06-10 16:23:09 +0200

    Create natural.h

---
+++ natural.h
@@ -0,0 +1,15 @@
+#ifndef NATURAL_H
+#define NATURAL_H
+
+class Natural {
+public:
+    int value;
+
+    Natural(int v) : value(v) {}
+
+    int getValue() {
+        return value;
+    }
+};
+
+#endif
